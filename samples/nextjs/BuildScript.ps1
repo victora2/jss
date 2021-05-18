0dc268bf0893b4ccb030fca8ac0ed2941e14c6a0
mykc <mykola.cherniak@sitecore.com>
2021-05-18 16:18:30 +0300

    Update BuildScript.ps1

--- samples/nextjs/BuildScript.ps1
+++ samples/nextjs/BuildScript.ps1
@@ -1,3 +1,3 @@
-﻿jss setup --instancePath $SITECORE_API_HOST --apiKey $SITECORE_API_KEY --deployUrl "$SITECORE_API_HOST/sitecore/api/jss/import" --deploySecret $JSS_DEPLOY_SECRET --nonInteractive --skipValidation | Out-Null;
+﻿jss setup --instancePath $SITECORE_API_HOST --apiKey $SITECORE_API_KEY --deployUrl "$SITECORE_API_HOST/sitecore/api/jss/import" --deploySecret $JSS_DEPLOY_SECRET --nonInteractive --skipValidation;
 jss deploy items --includeContent --includeDictionary;
 npm run build;
