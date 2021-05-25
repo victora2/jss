8cc9f447375e5fdb72adae24db6c56cd748522b0
mykc <82385082+sc-mykolacherniak@users.noreply.github.com>
2021-05-25 14:16:59 +0300

    Delete BuildScript.ps1

--- samples/nextjs/BuildScript.ps1
+++
@@ -1,3 +0,0 @@
-﻿npm install -g @sitecore-jss/sitecore-jss-cli;
-jss setup --layoutServiceHost $SITECORE_API_HOST --apiKey $SITECORE_API_KEY --deployUrl "$SITECORE_API_HOST/sitecore/api/jss/import" --deploySecret $JSS_DEPLOY_SECRET --nonInteractive --skipValidation;
-jss deploy items --includeContent --includeDictionary;
