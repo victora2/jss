9d1e4a6de2523b812ef34a760c036b2061290523
Victor Antofica <victor.antofica@valtech.com>
2021-09-26 19:51:07 +0200

    Add manifest for Contact comp. scaffolding

--- samples/vue/scripts/scaffold-component.js
+++ samples/vue/scripts/scaffold-component.js
@@ -156,8 +156,39 @@ export default {
   return outputFilePath;
 }
 
+function getContactManifestTemplate(){
+  return `// eslint-disable-next-line no-unused-vars
+  import { CommonFieldTypes, SitecoreIcon, Manifest } from '@sitecore-jss/sitecore-jss-manifest';
+
+  /**
+   * Adds the ${componentName} component to the disconnected manifest.
+   * This function is invoked by convention (*.sitecore.js) when 'jss manifest' is run.
+   * @param {Manifest} manifest Manifest instance to add components to
+   */
+  export default function(manifest) {
+    manifest.addComponent({
+      name: '${componentName}',
+      icon: SitecoreIcon.DocumentTag,
+      fields: [{ name: 'heading', type: CommonFieldTypes.SingleLineText }],
+      fields: [{ name: 'firstName', type: CommonFieldTypes.SingleLineText }],
+      fields: [{ name: 'lastName', type: CommonFieldTypes.SingleLineText }],
+      fields: [{ name: 'email', type: CommonFieldTypes.SingleLineText }],
+      /*
+      If the component implementation uses the 'Placeholder' component to expose a placeholder,
+      register it here, or components added to that placeholder will not be returned by Sitecore:
+      placeholders: ['exposed-placeholder-name']
+      */
+    });
+  }
+  `;
+}
+
 function scaffoldManifest() {
-  const manifestTemplate = `// eslint-disable-next-line no-unused-vars
+  let manifestTemplate  = null;
+  if(componentName == 'contact')
+    manifestTemplate = getContactManifestTemplate()
+  else
+    manifestTemplate = `// eslint-disable-next-line no-unused-vars
 import { CommonFieldTypes, SitecoreIcon, Manifest } from '@sitecore-jss/sitecore-jss-manifest';
 
 /**
